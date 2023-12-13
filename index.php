70a973624511b60864cc9e717336896184d6bdca
oussama <znagui.info@gmail.com>
2023-12-13 21:44:02 +0100

    page creer un compte

--- index.php
+++ index.php
@@ -11,52 +11,50 @@
         <h1 class ='text-lg font-black text-white text-center my-5 lg:text-2xl'>
             Rejoignez notre Jardin Vert: Inscrivez-vous pour Cultiver des Moments de Beauté Botanique
         </h1>
-        <form class="w-full max-w-lg bg-gray-300 p-10 rounded-xl">
+        <form id="form" class="w-full max-w-lg bg-gray-300 p-10 rounded-xl">
   <div class="flex flex-wrap -mx-3">
     <div class="w-full md:w-1/2 px-3  md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         Prenom
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" placeholder="Jane">
       <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Nom
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="last-name" type="text" placeholder="Doe">
     </div>
   </div>
  
-    <div class="w-full px-3">
+    <div class="w-full my-3  ">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Email
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="email.pro@mail.com">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="mail" type="email" placeholder="email.pro@mail.com">
     </div>
-    <div class="w-full px-3">
+    <div class="w-full ">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Mot de pass
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="password" type="password" placeholder="******************">
     </div>
-    <div class="w-full px-3">
+    <div class="w-full ">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Confirmez le mot de pass
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
     </div>
     <div class="w-full  flex justify-center items-center">
-      
-      <input class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded " value="Valider" id="grid-password" type="submit" >
-    </div>
-    
-  
-  
+      <input class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded " value="Valider" type="submit" id = 'go'>    
+    </div>  
 </form>
+   
+
         
 
     </header>
-    
+
 </body>
 </html>
---
+++ main.js
@@ -0,0 +1,12 @@
+let nom = document.getElementById("first-name").value;
+let form = document.getElementById("form");
+let prenom = document.getElementById("last-name");
+let mail = document.getElementById("mail");
+let pass = document.getElementById("password");
+let btn = document.getElementById("go");
+
+btn.addEventListener("click", regex);
+
+function regex() {
+  console.log("aloo");
+}
