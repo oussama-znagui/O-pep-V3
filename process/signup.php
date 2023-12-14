66947aa24f9ca1f90f2c9a525c590de96d1df5a7
oussama <znagui.info@gmail.com>
2023-12-14 11:47:52 +0100

    page creer un compte

--- process/signup.php
+++ process/signup.php
@@ -1,12 +1,16 @@
 <?php
-include 'config.php'
-$nom = $_POST['nom'];
-$pnom = $_POST['Pnom'];
+if(!$_POST){
+    header('Location: ../index.php');
+}
+include '../config.php';
+$nom = $_POST['nom'] . ' ' .$_POST['Pnom'];
+
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 $cpass = $_POST['cpass'];
-if(empty($nom) || empty($pnom)){
-    die('veuillez saisir votre nom et votre prenom');
+if(empty($_POST['nom']) || empty($_POST['Pnom'])){
+    ('veuillez saisir votre nom et votre prenom');
+
 }
 if(! filter_var($email, FILTER_VALIDATE_EMAIL)){
      die('veuillez saisir un email valide');
@@ -14,4 +18,6 @@ if(! filter_var($email, FILTER_VALIDATE_EMAIL)){
 if($pass != $cpass){
     die('les mots de pass ne sont pas identiques');
 }
+
+$sql = "INSERT INTO utilisateur (nom,email,passwordUser) VALUES (?,?,?)";
 
